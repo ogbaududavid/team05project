cc44c8336d4b497ce2dee297e5b43c6dd0342032
David Adeogun <adeogundavidt@gmail.com>
2024-12-12 21:46:36 -0500

    updated due date to datetime

--- obj/Debug/net8.0/team_project.AssemblyInfo.cs
+++ obj/Debug/net8.0/team_project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("team_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56a2a70850d017825964ef205d995ca6f87d3725")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ebf30ba8a48cea0f35b477c16071ca6456c11cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("team_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("team_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
