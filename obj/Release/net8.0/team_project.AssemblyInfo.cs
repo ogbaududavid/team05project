42ba3a7976879660a1467edbd9dfbcda59b7310e
David Adeogun <adeogundavidt@gmail.com>
2024-12-15 13:45:44 -0500

    port

--- obj/Release/net8.0/team_project.AssemblyInfo.cs
+++ obj/Release/net8.0/team_project.AssemblyInfo.cs
@@ -11,9 +11,9 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("team_project")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69eea3048fbe118d6d3ebffdce3dcea98eab40be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddd9bccbabf3a9687f4396414103a9e76158a373")]
 [assembly: System.Reflection.AssemblyProductAttribute("team_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("team_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
