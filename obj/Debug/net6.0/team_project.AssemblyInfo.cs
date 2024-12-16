9797b033f33825a0510660bdb4b348dac21e6d4d
ogbaududavid <ogbaudu.dc@gmail.com>
2024-12-16 10:35:24 +0100

    Removed some adjustments

--- obj/Debug/net6.0/team_project.AssemblyInfo.cs
+++ obj/Debug/net6.0/team_project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("team_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cfbb7658084e8862f6e15ea5c326fec4243a171")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+183f15cb84ebefa3caf7e757eeb5ecb67f75f638")]
 [assembly: System.Reflection.AssemblyProductAttribute("team_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("team_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
