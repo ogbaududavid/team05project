6f53373bafd784431144f04b5f7cd32bd40b80c3
ogbaududavid <ogbaudu.dc@gmail.com>
2024-12-16 10:38:34 +0100

    Reverted back to 6.0

--- obj/Debug/net6.0/team_project.AssemblyInfo.cs
+++ obj/Debug/net6.0/team_project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("team_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+183f15cb84ebefa3caf7e757eeb5ecb67f75f638")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9797b033f33825a0510660bdb4b348dac21e6d4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("team_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("team_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
