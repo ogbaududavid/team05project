c813420a97bf9681559d91e0a015b3591e819d63
ogbaududavid <ogbaudu.dc@gmail.com>
2024-12-16 10:08:43 +0100

    Created custom configuration file fot netlify

--- obj/Debug/net8.0/team_project.AssemblyInfo.cs
+++ obj/Debug/net8.0/team_project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("team_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c4567bbfe605fb9aa2628bf1f674ad2f3457d5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+961af4550ed8344e1ff879deb6d7aa8090deb117")]
 [assembly: System.Reflection.AssemblyProductAttribute("team_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("team_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
