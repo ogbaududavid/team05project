edad69a25694ac57f84766d2bbb2e04139ab7a04
ogbaududavid <ogbaudu.dc@gmail.com>
2024-12-17 23:06:31 +0100

    renamed  docker file

--- obj/Debug/net8.0/team_project.AssemblyInfo.cs
+++ obj/Debug/net8.0/team_project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("team_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+216882315935f6269009cab6419b7a287df69780")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b5309bab97e530f111f95b6216eee41ecf70250")]
 [assembly: System.Reflection.AssemblyProductAttribute("team_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("team_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
